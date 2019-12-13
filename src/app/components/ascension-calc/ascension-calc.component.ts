80eb8fb710bdec18a9b408883a4245ea2fa9ac31
Marciano <MarcianoG@users.noreply.github.com>
2019-12-13 15:34:09 +0100

    Calculations work but they only take into account the star and rarity you give them this also means I have to create some sort of list which lists the keys you are allowed to use when you for instance click 3 (also 2 and 1) when 4 (also 3 2 and 1). I am not happy with the way it is coded :(

--- src/app/components/ascension-calc/ascension-calc.component.ts
+++ src/app/components/ascension-calc/ascension-calc.component.ts
@@ -15,28 +15,91 @@ export class AscensionCalcComponent implements OnInit {
   epic: Map<string, number>;
   legendary: Map<string, number>;
 
+  arcane: Map<string, number>;
+  affinity: Map<string, number>;
+
   helpOverlayIsActive: boolean;
 
   constructor() {
     this.xpInfo = rankInfo['default'];
-    this.uncommon = new Map<string, number>().set('three', 0).set('four', 0).set('five', 0).set('six', 0);
-    this.rare = new Map<string, number>([['three', 0], ['four', 0], ['five', 0], ['six', 0]]);
+    this.uncommon = new Map<string, number>();
+    this.rare = new Map<string, number>();
     this.epic = new Map<string, number>();
-    this.epic['three'] = 0
-    this.epic['four'] = 0
-    this.epic['five'] = 0
-    this.epic['six'] = 0
-    // this.legendary = new Map<string, number>([['three', 0], ['four', 0], ['five', 0], ['six', 0]]);
-    console.log(this.uncommon);
-    console.log(this.rare);
-    console.log(this.epic)
+    this.legendary = new Map<string, number>();
+    this.arcane = new Map<string, number>();
+    this.affinity = new Map<string, number>();
+    this.initialise();
   }
 
   ngOnInit() {
   }
 
   calculatePotions(){
-    console.log("Calls this method")
+    this.resetPotions();
+    this.calculatePotionsPerAff(this.uncommon, 'uncommon');
+    this.calculatePotionsPerAff(this.rare, 'rare');
+    this.calculatePotionsPerAff(this.epic, 'epic');
+    this.calculatePotionsPerAff(this.legendary, 'legendary');
+    console.log(this.arcane);
+    console.log(this.affinity);
+  }
+
+  calculatePotionsPerAff(requesting: Map<string, number>, rarity: string){
+    let totalArcane = 0;
+    let totalAffinity = 0;
+    for(let key in this.xpInfo){
+      if(key in requesting && requesting[key] > 0){
+        totalArcane += this.xpInfo[key].ascension[rarity].arcane;
+        totalAffinity += this.xpInfo[key].ascension[rarity].affinity;
+      }
+    }
+    if (totalArcane > 0 && totalAffinity > 0){
+      this.arcane['superior'] += Math.floor(totalArcane/100);
+      totalArcane -= Math.floor(totalArcane/100);
+      this.arcane['greater'] += Math.floor(totalArcane/10);
+      totalArcane -= Math.floor(totalArcane/10);
+      this.arcane['lesser'] += totalArcane;
+      
+      this.affinity['superior'] += Math.floor(totalAffinity/100);
+      totalAffinity -= Math.floor(totalAffinity/100);
+      this.affinity['greater'] += Math.floor(totalAffinity/10);
+      totalAffinity -= Math.floor(totalAffinity/10);
+      this.affinity['lesser'] += totalAffinity;
+    }
+  }
+
+  initialise(){
+    this.uncommon['three'] = 0;
+    this.uncommon['four'] = 0;
+    this.uncommon['five'] = 0;
+    this.uncommon['six'] = 0;
+
+    this.rare['three'] = 0;
+    this.rare['four'] = 0;
+    this.rare['five'] = 0;
+    this.rare['six'] = 0;
+
+    this.epic['three'] = 0;
+    this.epic['four'] = 0;
+    this.epic['five'] = 0;
+    this.epic['six'] = 0;
+
+    this.legendary['three'] = 0;
+    this.legendary['four'] = 0;
+    this.legendary['five'] = 0;
+    this.legendary['six'] = 0;
+
+    this.resetPotions();
+  }
+
+  resetPotions(){
+    this.arcane['lesser'] = 0;
+    this.arcane['greater'] = 0;
+    this.arcane['superior'] = 0;
+
+    this.affinity['lesser'] = 0;
+    this.affinity['greater'] = 0;
+    this.affinity['superior'] = 0;
   }
 
   helpOverlay() {
