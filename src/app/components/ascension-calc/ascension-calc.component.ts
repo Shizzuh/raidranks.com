ad9c66ee47c91f16b38f74811619eec62f583ffc
Marciano <MarcianoG@users.noreply.github.com>
2019-12-13 14:14:47 +0100

    Have no clue why it works like this all the maps are the same but Angular will only take the one initialised as this.epic

--- src/app/components/ascension-calc/ascension-calc.component.ts
+++ src/app/components/ascension-calc/ascension-calc.component.ts
@@ -10,11 +10,33 @@ export class AscensionCalcComponent implements OnInit {
 
   xpInfo: any;
 
+  uncommon: Map<string, number>;
+  rare: Map<string, number>;
+  epic: Map<string, number>;
+  legendary: Map<string, number>;
+
   helpOverlayIsActive: boolean;
 
-  constructor() { }
+  constructor() {
+    this.xpInfo = rankInfo['default'];
+    this.uncommon = new Map<string, number>().set('three', 0).set('four', 0).set('five', 0).set('six', 0);
+    this.rare = new Map<string, number>([['three', 0], ['four', 0], ['five', 0], ['six', 0]]);
+    this.epic = new Map<string, number>();
+    this.epic['three'] = 0
+    this.epic['four'] = 0
+    this.epic['five'] = 0
+    this.epic['six'] = 0
+    // this.legendary = new Map<string, number>([['three', 0], ['four', 0], ['five', 0], ['six', 0]]);
+    console.log(this.uncommon);
+    console.log(this.rare);
+    console.log(this.epic)
+  }
 
   ngOnInit() {
+  }
+
+  calculatePotions(){
+    console.log("Calls this method")
   }
 
   helpOverlay() {
