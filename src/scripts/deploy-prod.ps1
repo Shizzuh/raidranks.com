799357bfcc33a68ed24c9ebb5d8280c62ab2e317
Paul Sullivan <pwsm50@gmail.com>
2019-12-20 13:32:45 -0600

    WIP Commit

---
+++ src/scripts/deploy-prod.ps1
@@ -0,0 +1,11 @@
+$currentDir = Get-Location
+
+Set-Location -Path ../../ -PassThru
+
+ng build --prod
+mkdir dist\rsl\.well-known
+cp -r assetlinks.json dist\rsl\.well-known
+cp -r ads.txt dist\rsl
+firebase deploy --only hosting:raidranks
+
+Set-Location "$currentDir"
