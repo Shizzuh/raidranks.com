09841339c64f98de36e3881bc89e8d9fb868039a
Shizzuh <57287810+Shizzuh@users.noreply.github.com>
2019-12-21 19:12:31 +0100

    hmmm

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,8 @@ import { ChampionsService } from 'src/app/services/champions.service';
 import { Component, OnInit } from '@angular/core';
 import { Champion } from 'src/app/models/team.model';
 import * as _ from 'lodash';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'home',
@@ -21,81 +23,23 @@ export class HomeComponent implements OnInit {
   showBluestacks: boolean;
   bluestacksCount: number;
   adActive: boolean;
+  news: any;
 
   selected = 'option2';
 
   constructor(
-    private championsService: ChampionsService,
-    private localStorage: Storage
+    private db: AngularFirestore
   ) { }
 
   ngOnInit() {
-    this.showBluestacks = false;
-    this.bluestacksCount = +localStorage.getItem('bluestacksCount');
-    if (this.bluestacksCount && this.bluestacksCount === 10) {
-      this.showBluestacks = true;
-    }
-    this.onCoolDown = false;
-    this.championsList = this.championsService.getAllChampions();
-    this.legendaryChampions = _.filter(this.championsList, ['rarity', 'Legendary']);
-    this.epicChampions = _.filter(this.championsList, ['rarity', 'Epic']);
-    this.rareChampions = _.filter(this.championsList, ['rarity', 'Rare']);
-    this.adActive = true;
+    this.news = this.db.collection('newsthumb');
+    this.news = this.news.snapshotChanges()
+      .pipe(
+        map((actions: any) => actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        }))
+      );
   }
-
-  summon(shardCount: number, chances: number) {
-    this.bluestacksCount++;
-    localStorage.setItem('bluestacksCount', this.bluestacksCount + '');
-    this.bluestacksCount = +localStorage.getItem('bluestacksCount');
-    if (this.bluestacksCount && this.bluestacksCount === 10) {
-      this.showBluestacks = true;
-      return;
-    } else {
-      this.showBluestacks = false;
-    }
-    this.onCoolDown = true;
-    this.randomChamps = [];
-    for (let i = 1; i <= shardCount; i++ ) {
-      if (this.getRandomNumber() <= 0.5 * chances) {
-        this.getRandomChampionIndex(this.legendaryChampions);
-      } else if (this.getRandomNumber() <= 8 * chances) {
-        this.getRandomChampionIndex(this.epicChampions);
-      } else {
-        this.getRandomChampionIndex(this.rareChampions);
-      }
-    }
-    setTimeout(() => {
-      this.onCoolDown = false;
-    }, 10000);
-    this.adActive = false;
-    setTimeout(() => {
-      this.adActive = true;
-    }, 1);
-  }
-
-  getRandomNumber() {
-    return Math.round(Math.random() * 10000) / 100;
-  }
-
-  getRandomChampionIndex(champions: Champion[]) {
-    let randomIndex =  Math.round(Math.random() * champions.length - 1);
-    randomIndex < 0 ? randomIndex = 0 : randomIndex = randomIndex;
-    if (champions[randomIndex].element === undefined) {
-    }
-    this.randomChamps.push(champions[randomIndex]);
-  }
-
-  closeBluestacks(resetCount: boolean) {
-    this.showBluestacks = false;
-    if (!resetCount) {
-      localStorage.setItem('bluestacksCount', this.bluestacksCount + '');
-      this.bluestacksCount = +localStorage.getItem('bluestacksCount');
-      return;
-    }
-    if (resetCount) {
-      localStorage.setItem('bluestacksCount', '0');
-      this.bluestacksCount = +localStorage.getItem('bluestacksCount');
-    }
-  }
-
 }
